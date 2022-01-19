1a561ae2883fca2bd723e58374a8c72e34921c2d
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-19 03:38:31 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput,Button , ScrollView  } from 'react-native';
+import Start from './components/Start';
+import Chat from './components/Chat';
+// import react native gesture handler
+import 'react-native-gesture-handler';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+export default class App extends React.Component {
+/*  constructor(props) {
+   super(props);
+   this.state = { text: '' };
+ }
+ alertMyText (input = []) {
+  Alert.alert(input.text);
+}
+ render() {
+   return (
+    <NavigationContainer>
+     <View style={{flex:1, justifyContent:'center'}}>
+       <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         onChangeText={(text) => this.setState({text})}
+         value={this.state.text}
+         placeholder='Type here ...'
+       />
+       <Text>You wrote: {this.state.text}</Text>
+       <Button
+  onPress={() => {
+    this.alertMyText({text: this.state.text});
+  }}
+  title="Press Me"
+/>
+
+     </View>
+     </NavigationContainer>
+   );
+ }*/
+
+ render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
