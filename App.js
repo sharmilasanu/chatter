0dc4ff8e9464c926ea213089e380e18b1a42915b
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-22 14:12:45 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,45 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput,Button , ScrollView  } from 'react-native';
-import Start from './components/Start';
-import Chat from './components/Chat';
+
 // import react native gesture handler
 import 'react-native-gesture-handler';
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Start from './components/Start';
+import Chat from './components/Chat';
 
 const Stack = createStackNavigator();
 export default class App extends React.Component {
-/*  constructor(props) {
-   super(props);
-   this.state = { text: '' };
- }
- alertMyText (input = []) {
-  Alert.alert(input.text);
-}
- render() {
-   return (
-    <NavigationContainer>
-     <View style={{flex:1, justifyContent:'center'}}>
-       <TextInput
-         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-         onChangeText={(text) => this.setState({text})}
-         value={this.state.text}
-         placeholder='Type here ...'
-       />
-       <Text>You wrote: {this.state.text}</Text>
-       <Button
-  onPress={() => {
-    this.alertMyText({text: this.state.text});
-  }}
-  title="Press Me"
-/>
-
-     </View>
-     </NavigationContainer>
-   );
- }*/
-
  render() {
   return (
     <NavigationContainer>
